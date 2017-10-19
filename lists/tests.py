8494bfa77b9449977f04f55092d31fd6307842ce
MadCatMat <mathieum@vialogica.com>
2017-10-19 18:27:17 -0400

    Created the List model and set it as a ForeignKey on the Item model. Migrated DB. Modified tests to account for new List model.

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,6 @@
 
 from django.test import TestCase
-from lists.models import Item
+from lists.models import Item, List
 
 class HomePageTest(TestCase):
     def test_uses_home_template(self):
@@ -11,16 +11,24 @@ class HomePageTest(TestCase):
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
 
-class ItemModelTests(TestCase):
+class ListAndItemModelTests(TestCase):
 
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = 'The first (ever) list item'
+        first_item.user_list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Item the second'
+        second_item.user_list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(),2)
@@ -28,7 +36,9 @@ class ItemModelTests(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.user_list, list_)
         self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.user_list, list_)
 
 class ListViewTests (TestCase):
     def test_uses_list_template(self):
@@ -36,8 +46,9 @@ class ListViewTests (TestCase):
         self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_all_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1', user_list=list_)
+        Item.objects.create(text='itemey 2', user_list=list_)
 
         response = self.client.get('/lists/the-only-list-in-the-world/')
 
